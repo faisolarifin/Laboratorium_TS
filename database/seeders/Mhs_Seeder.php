184795bee71e187ba97df958de9a6dba5f4f323d
Ach. Faisol S. Arifin <faisolofficial99@gmail.com>
2022-07-14 22:28:34 +0700

    update

--- database/seeders/Mhs_Seeder.php
+++ database/seeders/Mhs_Seeder.php
@@ -7,6 +7,7 @@ use App\Models\AkunMhs;
 use App\Models\Dosen;
 use App\Models\Setting;
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 
 class Mhs_Seeder extends Seeder
 {
@@ -17,16 +18,17 @@ class Mhs_Seeder extends Seeder
      */
     public function run()
     {
+        $faker = Factory::create('id_ID');
         for($i=0; $i < 50; $i++) {
             AkunMhs::create([
-                        'nama' => fake()->name(),
+                        'nama' => $faker->name(),
                         'nim' => rand(11111111,99999999),
                         'password' => 'faisol',
-                        'alamat' => fake()->address(),
-                        'tmp_lahir' => fake()->city(),
-                        'tgl_lahir' => fake()->date(),
-                        'no_hp' => fake()->phoneNumber(),
-                        'email' => fake()->unique()->safeEmail(),
+                        'alamat' => $faker->address(),
+                        'tmp_lahir' => $faker->city(),
+                        'tgl_lahir' => $faker->date(),
+                        'no_hp' => $faker->phoneNumber(),
+                        'email' => $faker->unique()->safeEmail(),
                         'foto' => 'public/foto/default.png',
                         'status' => 'non-aktif',
                     ]);
@@ -35,11 +37,11 @@ class Mhs_Seeder extends Seeder
         for($i=0; $i < 15; $i++) {
             Dosen::create([
                 'nip' => rand(111111111,999999999),
-                'nama' => 'Dr. '.fake()->name(),
-                'jabatan' => fake()->jobTitle(),
-                'alamat' => fake()->address(),
-                'no_hp' => fake()->phoneNumber(),
-                'email' => fake()->safeEmail(),
+                'nama' => 'Dr. '.$faker->name(),
+                'jabatan' => $faker->jobTitle(),
+                'alamat' => $faker->address(),
+                'no_hp' => $faker->phoneNumber(),
+                'email' => $faker->safeEmail(),
             ]);
         }
 
